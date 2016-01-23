368bb6484d9b4dc7f335a3ea127ef9f339a3bb50
kdevouassoux <kilian.devouassoux@gmail.com>
2016-01-23 11:22:07 +0100

    Add useful routes for the app.

--- app/routes.php
+++ app/routes.php
@@ -2,5 +2,29 @@
 
 // Home page
 $app->get('/', function () use ($app) {
-  return $app['twig']->render('hello.twig', array());
+  return $app['twig']->render('home.twig', array());
+});
+
+// Teams list
+$app->get('/teams', function () use ($app) {
+    return $app['twig']->render('teams.twig', array());
+});
+
+// Teammates list
+$app->get('/teammates', function () use ($app) {
+    return $app['twig']->render('teammates.twig', array());
+});
+
+// Team detail
+$app->get('/team/{id}', function ($id) use ($app) {
+    return $app['twig']->render('team.twig', array(
+        'id' => $id
+    ));
+});
+
+// Teammate detail
+$app->get('/teammate/{id}', function ($id) use ($app) {
+    return $app['twig']->render('teammate.twig', array(
+        'id' => $id
+    ));
 });
